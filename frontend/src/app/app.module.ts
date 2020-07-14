e6fa60c284e0f5352e0d9c866c9e6aab345dc9f4
Raphaela <raphaelabarreto96@gmail.com>
2020-07-13 22:24:25 -0300

    resolução de problemas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,19 +2,22 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-import {ButtonModule, CardModule, InputTextModule, TableModule} from 'primeng';
+import {ButtonModule, CardModule, InputTextModule, MenuModule, SidebarModule, TableModule} from 'primeng';
 
 import {AppComponent} from './app.component';
 import {AtorComponent} from './pages/ator/form/ator.component';
 import {ClasseComponent} from './pages/classe/form/classe.component';
 import {DiretorComponent} from './pages/diretor/form/diretor.component';
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     AtorComponent,
     DiretorComponent,
-    ClasseComponent
+    ClasseComponent, SidebarComponent
+
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,10 @@ import {DiretorComponent} from './pages/diretor/form/diretor.component';
     ButtonModule,
     InputTextModule,
     CardModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
