e23d9f4f508d00817b7b05818063c76e861bf3c9
Gustavo Zucolotto <guguzucoloto@gmail.com>
2020-07-15 10:08:36 -0300

    refatoração do diretor

--- frontend/src/app/pages/diretor/form/diretor.component.ts
+++ frontend/src/app/pages/diretor/form/diretor.component.ts
@@ -9,29 +9,30 @@ import {DiretorService} from '../service/diretor.service';
 })
 export class DiretorComponent implements OnInit {
 
+  public diretores: Diretor[];
+
   public novoDiretor: Diretor;
   public diretorSelecionado: Diretor;
-  public diretores: Diretor[];
 
   constructor(private diretorService: DiretorService) {
   }
 
   ngOnInit(): void {
-    this.diretorService.getAll().subscribe(value => this.diretores = value);
-    this.novoDiretor = new Diretor();
+    this.diretorService.getAll().subscribe(diretores => this.diretores = diretores);
+    this.initialize();
   }
 
   delete(diretorSelecionado: Diretor) {
     this.diretorService.delete(diretorSelecionado.id).subscribe(() => {
-      this.diretores = this.diretores.filter(value => value.id !== diretorSelecionado.id);
-      this.diretorSelecionado = null;
+      this.diretores = this.diretores.filter(diretor => diretor.id !== diretorSelecionado.id);
+      this.cleanSelection();
     });
   }
 
   post(novoDiretor: Diretor) {
-    this.diretorService.post(novoDiretor).subscribe(value => {
-      this.diretores.push(value);
-      this.novoDiretor = new Diretor();
+    this.diretorService.post(novoDiretor).subscribe(diretorRegistrado => {
+      this.diretores.push(diretorRegistrado);
+      this.initialize();
     });
   }
 
@@ -41,6 +42,15 @@ export class DiretorComponent implements OnInit {
 
   setFocus() {
     document.getElementById('inputNome').focus();
+    this.cleanSelection();
+  }
+
+  cleanSelection() {
+    this.diretorSelecionado = null;
+  }
+
+  initialize() {
+    this.novoDiretor = new Diretor();
   }
 
 }
